1eb596195125d13a955843019ad034e22db28750
heritier-kodehode <heritier.kodehode@gmail.com>
2022-12-12 10:51:31 +0100

    first commit with navbar react router setup

---
+++ src/pages/climatecompensation/climatecompensation.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function climatecompensation() {
+  return <div>climatecompensation</div>;
+}
+
+export default climatecompensation;
---
+++ src/pages/vision/vision.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function vision() {
+  return <div>vision</div>;
+}
+
+export default vision;
