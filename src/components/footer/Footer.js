58a0994b99e2d9cfe03c776f28c5d1fce824585b
heritier-kodehode <heritier.kodehode@gmail.com>
2022-12-19 09:51:31 +0100

    first commit on footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
+import logo from '../../images/menulogo.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAnglesRight } from '@fortawesome/free-solid-svg-icons';
 
 const FooterContainer = styled.footer`
   margin-top: auto;
+  height: 20vh;
+  background-color: #21212d;
 `;
 
+const FooterInfoContainer = styled.div``;
+
+const FooterCopyRightContainer = styled.div``;
+
+const FooterInfoSection = styled.div``;
+
+const FooterContactSection = styled.div``;
+
+const FooterLogo = styled.img``;
+
+const FooterSocialIcon = styled(FontAwesomeIcon)``;
+
 function Footer() {
-  return <FooterContainer>Footer</FooterContainer>;
+  return (
+    <FooterContainer>
+      <FooterInfoContainer>
+        <FooterInfoSection></FooterInfoSection>
+        <FooterCopyRightContainer>
+          <p>Copyrights © 2022 Medretur. Designed by Heritier Z.A</p>
+        </FooterCopyRightContainer>
+      </FooterInfoContainer>
+    </FooterContainer>
+  );
 }
 
 export default Footer;
