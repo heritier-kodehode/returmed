5d83aad4efc8a5b83812923e97f59947e6fdd631
heritier-kodehode <heritier.kodehode@gmail.com>
2023-01-02 14:04:29 +0100

    Stylized links to partners

--- src/pages/partners/partners.js
+++ src/pages/partners/partners.js
@@ -4,7 +4,7 @@ import { Context } from '../../App';
 
 const links = [
   { name: 'Norges forsknigsråd', link: 'https://www.forskningsradet.no/' },
-  { name: 'Sjulkehusapoteka Vest', link: 'https://sjukehusapoteka-vest.no/' },
+  { name: 'Sjukehusapoteka Vest', link: 'https://sjukehusapoteka-vest.no/' },
   { name: 'Høgskole på Vestlandet', link: 'https://www.hvl.no/' },
   { name: 'Haugaland Interkommunale Miljøverk IKS', link: 'https://him.as/' },
   {
@@ -16,12 +16,14 @@ const links = [
 
 const PartnersContainer = styled.div`
   width: 100%;
-  height: 30vh;
+  height: auto;
   display: flex;
   flex-direction: column;
-  margin: 0 auto;
+  margin: 7rem auto;
   @media (min-width: 990px) {
     margin-top: 100px;
+    flex-direction: row;
+    flex-wrap: wrap;
   }
 `;
 
@@ -44,13 +46,24 @@ const PartnerLinkContainer = styled.a`
   width: 100%;
   cursor: pointer;
   text-decoration: none;
-  background-color: #6abe52;
+  background-color: #379237;
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: 2rem;
+  margin: 1.5rem auto;
+  padding: 7rem;
+  color: white;
+  text-decoration: underline;
+
+  &:hover {
+    background-color: #6abe52;
+    text-decoration: wavy underline;
+    text-decoration-thickness: 2px;
+    transform: translateY(-3px);
+  }
   @media (min-width: 990px) {
-    width: 10rem;
+    width: 30rem;
   }
 `;
 
@@ -61,6 +74,21 @@ function Partners() {
       <PartnersTitle>{langData.partners[0]}</PartnersTitle>
       <PartnerLinkContainer href={links[0].link}>
         {links[0].name}
+      </PartnerLinkContainer>
+      <PartnerLinkContainer href={links[1].link}>
+        {links[1].name}
+      </PartnerLinkContainer>
+      <PartnerLinkContainer href={links[2].link}>
+        {links[2].name}
+      </PartnerLinkContainer>
+      <PartnerLinkContainer href={links[3].link}>
+        {links[3].name}
+      </PartnerLinkContainer>
+      <PartnerLinkContainer href={links[4].link}>
+        {links[4].name}
+      </PartnerLinkContainer>
+      <PartnerLinkContainer href={links[5].link}>
+        {links[5].name}
       </PartnerLinkContainer>
     </PartnersContainer>
   );
